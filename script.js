82e55afec179fd8c8c2cb63d70b4d2009f167d6c
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-10-15 21:07:44 +0600

    multiple donation function done

--- script.js
+++ script.js
@@ -27,22 +27,44 @@ const history = () => {
     "bg-[#B4F461] text-white font-semibold w-[150px] h-[49px] rounded-xl";
 };
 
-let count = 0;
-const handleDonation = () => {
-  const donInput = document.getElementById("don-input");
-  let donAmount = document.getElementById("don-amount");
+const allButtons = document.querySelectorAll("#donation button");
+for (let btn of allButtons) {
+  btn.addEventListener("click", function (event) {
+    const btnId = btn.id;
+    const inputId = event.target.parentNode.querySelector("input").id;
+    const donAmount = event.target.parentNode.querySelector(".don-amount");
+    let donAmountNum = parseInt(donAmount.innerText);
+    handleDonation(donAmount, inputId, donAmountNum, event);
+  });
+}
 
+const handleDonation = (donAmount, inputId, donAmountNum, event) => {
+  const donInput = document.getElementById(inputId);
   const donationValue = parseInt(donInput.value);
   if (isNaN(donationValue)) {
     alert("Please enter a valid number");
     return;
   }
 
-  count = count + donationValue;
-  donAmount.innerText = count;
+  donAmountNum = donAmountNum + donationValue;
+  donAmount.innerText = donAmountNum;
   donInput.value = "";
 
   const totalAmount = document.getElementById("total-amount");
   const totalAmountInt = parseInt(totalAmount.innerText);
   totalAmount.innerText = totalAmountInt - donationValue;
+
+  const history = document.getElementById("history");
+  const card = document.createElement("div");
+  card.classList = "border p-10 rounded-xl mb-5";
+  const title = event.target.parentNode.querySelector("h4").innerText;
+  card.innerHTML = `
+    <p class="text-2xl mb-5 font-semibold">
+          ${donAmountNum} Taka is ${title}
+        </p>
+        <p>
+      Date :  ${new Date()}
+        </p>
+        `;
+  history.appendChild(card);
 };
